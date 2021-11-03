6e46e4499b25e5e7f84e7aef5332f96a2ed4e260
Wallace Tácio <wallacett.dev@gmail.com>
2021-11-03 10:19:42 -0300

    Adiciona resolução ao exercício Senha ETEC 2021

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Senha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string senha, acesso;
+
+            Console.Write("Olá, usuário! Por favor, digite sua senha: ");
+            senha = Console.ReadLine();
+            Console.WriteLine();
+
+            if(senha == "1234abcd"){
+                Console.ForegroundColor = ConsoleColor.Green;
+                acesso = "Acesso permitido";
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                acesso = "Acesso negado";
+            }
+            Console.WriteLine($"{acesso}!");
+            Console.ResetColor();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Senha
-Exercício Senha ETEC 2021
+## Exercício `Senha` ETEC 2021
+
+Solicite ao usuário que digite a sua senha (uma string). Exiba "Acesso permitido" caso a senha digitada seja `1234abcd`, senão exiba "Acesso negado".
+
+Ex.:
+```
+Olá, usuário. Por favor, digite sua senha: minha_senha_super_secreta
+Acesso negado
+```
+ou
+```
+Olá, usuário. Por favor, digite sua senha: 1234abcd
+Acesso permitido
+```
